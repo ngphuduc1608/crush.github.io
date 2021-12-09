6f5b4250d8924ffcda879ca740b666ceb5432031
ngphuduc1608 <95741183+ngphuduc1608@users.noreply.github.com>
2021-12-09 11:05:18 +0700

    upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: ' Hee loo ! 💗',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Vâng',
+    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
+    btnYes: 'Có, em thích anh 💖💖💖',
+    btnNo: 'Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Ok ! 💌',
+    reply: 'Tại vì anh đẹp trai còn vui tính nữa ',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰.I Love you 3000 😘😘',
+    messDesc: ' Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Vâng anh',
+    messLink: 'https://m.me/ng.phuduc16'
+}
