67a04aa0afbd268a3a39d3991375dd8a9b70a8a0
Nguyễn Phú Đức <95741183+ngphuduc1608@users.noreply.github.com>
2025-02-22 22:21:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@ const CONFIG = {
     introTitle: ' Hee loo ! 💗',
     introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
     btnIntro: 'Vâng',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thích anh 💖💖💖',
+    title: 'Em ơi cho anh yêu em lần nữa nha',
+    desc: 'Em không trả lời hoặc thoát ra, tức là đồng ý đó nha :> ',
+    btnYes: 'Tạm tha cho anh đó ',
     btnNo: 'Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    question:'A biết lỗi rồi . I love u 3000 năm',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Ok ! 💌',
-    reply: 'Tại vì anh đẹp trai còn vui tính nữa ',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰.I Love you 3000 😘😘',
+    reply: 'Em yêu anh nhiều lắm ',
+    mess: 'Anh biết mà, Anh cũng yêu em lắm 🥰.I Love you 3000 😘. Bỏ qua cho anh nhaaa',
     messDesc: ' Nhắn tin cho Anh nha. 💬💬',
     btnAccept: 'Vâng anh',
-    messLink: 'https://m.me/ng.phuduc16'
+    messLink: 'https://m.me/quang.anh.779631'
 }
